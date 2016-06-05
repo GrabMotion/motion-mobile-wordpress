2a49f07fd4c1a58242bd1bd2668ae699f2a56c29
Jose Vigil <josemanuelvigil@gmail.com>
2016-06-04 21:08:10 -0300

    deleting unnecesary files

--- swift/GrabMotion_cplus_proto_mal/GrabMotion/GrabMotion-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#include <ifaddrs.h>
-#import <Parse/PFObject+Subclass.h>
-#import "CVWrapper.h"
-
-
